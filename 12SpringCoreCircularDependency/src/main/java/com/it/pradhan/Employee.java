5f4e2fdff4eda78fad918a8f9ee574d692d9e1bb
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2024-10-29 10:57:23 +0530

    SpringCoreCirDep
    Circular Dependency

---
+++ 12SpringCoreCircularDependency/src/main/java/com/it/pradhan/Employee.java
@@ -0,0 +1,31 @@
+package com.it.pradhan;
+
+public class Employee {
+
+	private String name;
+
+	private Project pob;
+
+	public Employee() {
+		super();
+		System.out.println("EMPLOYEE OBJ");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Project getPob() {
+		return pob;
+	}
+
+	public void setPob(Project pob) {
+		this.pob = pob;
+		System.out.println("EMPLOYEE.setProject ");
+	}
+
+}
